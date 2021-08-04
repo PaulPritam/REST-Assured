4978aedc3c89b0fe7f6c0c6f3854a3ea129835ca
unknown <pritampaul9797@gmail.com>
2021-08-04 16:13:53 +0530

    MODIFIED: Refactored the code and removed hard coded values

--- src/test/java/com/bridgelabz/restassured/RestAssured.java
+++ src/test/java/com/bridgelabz/restassured/RestAssured.java
@@ -1,33 +1,47 @@
 package com.bridgelabz.restassured;
 
 import io.restassured.http.ContentType;
+import io.restassured.internal.path.json.JSONAssertion;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.requestSpecification;
 
 public class RestAssured {
 
     public String token = "";
-    public static String userID = "";
-    public static String playlistId = "";
+    public static String userID ;
+    public static String playlistId ;
 
     @BeforeTest
     public void setUp() {
-        token = "Bearer BQCvpslyrXABonzzfDxjRlbEWb3EtygdB7_GKkmFRWXS9q0vn3jttA8ngAWcOSPe9Qh1hdtX9F0Eo-4Mgx6LLWG0tQBrKEvx" +
-                "z8m5rIN4YrbPmoxyhsrGPuTRfhIO3mLcbmwr8qauxTPjLQnxbcRDncaVmZhEAh-jptLqmkOGSTssLI46G5eJAkafpKMm6HpoFPzaSoX" +
-                "NlUlx-PEb1iUG-KiCWCNdiiqmN4dC_dH4qjMLD9J_X3P_4O0S0Ck11_kbd4R9mviIf0TOAjaFcmlxoqyM3SCWy6HVRvdMFblIcZW6";
+        token = "Bearer BQB48QYGAZDDUBA6LWM8bScGlvF0xYw6Foa7dlxdFjCNE8iyHmGrXvt7QMG0Y_4RBFo-2c--2610e12DZmXcZ-r2_OpWJ8xy" +
+                "i43r9waOvV0_5LvZnpTiewVsCEoMqB3dwq7Ng1M-g0iPfnv_ZCN8XioShnqoxpbqc3USoZJ2i2zOcw3DB81noun73ZXPovlmxjaICAG" +
+                "NkN8Duq2NKvMUwvZ1rBBMW_b6FlFHexfCzq8nuLAuHH0D0hPeM0eOIFwvVEXPfd3Wt8z_p4eBK0mO-IPpA_hR78krnD8sT8X3Lx0d";
     }
 
     /*
     Gets current profile data
      */
     @Test
-    public void get_Current_profile() {
+    public void get_Current_UserProfile() {
         Response response = given().contentType(ContentType.JSON)
                 .accept(ContentType.JSON).header("Authorization", token)
                 .when()
                 .get("https://api.spotify.com/v1/me");
+        response.prettyPrint();
+
+        userID = response.path("id");
+        System.out.println("Data from response-->"+userID);
+    }
+
+    @Test
+    public void get_UserProfile() {
+        Response response = given().contentType(ContentType.JSON)
+                .accept(ContentType.JSON).header("Authorization", token)
+                .when()
+                .get("https://api.spotify.com/v1/users/"+userID);
         response.prettyPrint();
     }
 
@@ -40,6 +54,9 @@ public class RestAssured {
                 .accept(ContentType.JSON).header("Authorization", token)
                 .when()
                 .get("https://api.spotify.com/v1/me/playlists");
+
+        playlistId = response.path("uri");
+        System.out.println("---"+playlistId);
         response.prettyPrint();
     }
 
@@ -47,11 +64,11 @@ public class RestAssured {
     Gets information on user's playlist cover image
      */
     @Test
-    public void get_CurrentUsersPlaylistCoverImage() {
+    public void get_APlaylistCover() {
         Response response = given().contentType(ContentType.JSON)
                 .accept(ContentType.JSON).header("Authorization", token)
                 .when()
-                .get("https://api.spotify.com/v1/playlists/6kARld7la7IvgtyUOt1eQX/images");
+                .get("https://api.spotify.com/v1/playlists/6kARld7la7IvgtyUOt1eQX");
         response.prettyPrint();
     }
 }
